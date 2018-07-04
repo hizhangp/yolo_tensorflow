fe4a1478f453b045b86425fee320080ee048c831
Sungjun Hong <imjune@yonsei.ac.kr>
2018-07-04 13:15:36 +0900

    refine load_pascal_annotation w/o loading an image

--- utils/pascal_voc.py
+++ utils/pascal_voc.py
@@ -24,35 +24,7 @@ class pascal_voc(object):
         self.epoch = 1
         self.gt_labels = None
         self.prepare()
-
-    def get(self):
-        images = np.zeros(
-            (self.batch_size, self.image_size, self.image_size, 3))
-        labels = np.zeros(
-            (self.batch_size, self.cell_size, self.cell_size, 25))
-        count = 0
-        while count < self.batch_size:
-            imname = self.gt_labels[self.cursor]['imname']
-            flipped = self.gt_labels[self.cursor]['flipped']
-            images[count, :, :, :] = self.image_read(imname, flipped)
-            labels[count, :, :, :] = self.gt_labels[self.cursor]['label']
-            count += 1
-            self.cursor += 1
-            if self.cursor >= len(self.gt_labels):
-                np.random.shuffle(self.gt_labels)
-                self.cursor = 0
-                self.epoch += 1
-        return images, labels
-
-    def image_read(self, imname, flipped=False):
-        image = cv2.imread(imname)
-        image = cv2.resize(image, (self.image_size, self.image_size))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
-        image = (image / 255.0) * 2.0 - 1.0
-        if flipped:
-            image = image[:, ::-1, :]
-        return image
-
+        
     def prepare(self):
         gt_labels = self.load_labels()
         if self.flipped:
@@ -72,7 +44,7 @@ class pascal_voc(object):
         np.random.shuffle(gt_labels)
         self.gt_labels = gt_labels
         return gt_labels
-
+        
     def load_labels(self):
         cache_file = os.path.join(
             self.cache_path, 'pascal_' + self.phase + '_gt_labels.pkl')
@@ -96,11 +68,11 @@ class pascal_voc(object):
                 self.data_path, 'ImageSets', 'Main', 'test.txt')
         with open(txtname, 'r') as f:
             self.image_index = [x.strip() for x in f.readlines()]
-
+            
         gt_labels = []
         for index in self.image_index:
-            label, num = self.load_pascal_annotation(index)
-            if num == 0:
+            label, obj_num = self.load_pascal_annotation(index)
+            if obj_num == 0:
                 continue
             imname = os.path.join(self.data_path, 'JPEGImages', index + '.jpg')
             gt_labels.append({'imname': imname,
@@ -110,27 +82,27 @@ class pascal_voc(object):
         with open(cache_file, 'wb') as f:
             pickle.dump(gt_labels, f)
         return gt_labels
-
+        
     def load_pascal_annotation(self, index):
-        """
-        Load image and bounding boxes info from XML file in the PASCAL VOC
-        format.
-        """
-
-        imname = os.path.join(self.data_path, 'JPEGImages', index + '.jpg')
-        im = cv2.imread(imname)
-        h_ratio = 1.0 * self.image_size / im.shape[0]
-        w_ratio = 1.0 * self.image_size / im.shape[1]
-        # im = cv2.resize(im, [self.image_size, self.image_size])
-
         label = np.zeros((self.cell_size, self.cell_size, 25))
+            # 25 = 1 + 4 + 20
+            # label[:, :, 0]    ==> occupied or not
+            # label[:, :, 1:5]  ==> cx, cy, w, h
+            # label[:, :, 5:]   ==> class indices
+            
         filename = os.path.join(self.data_path, 'Annotations', index + '.xml')
         tree = ET.parse(filename)
+        
+        size = tree.find('size')
+        width = float(size.find('width').text)
+        height = float(size.find('height').text)
+        w_ratio = 1.0 * self.image_size / width
+        h_ratio = 1.0 * self.image_size / height
+        
         objs = tree.findall('object')
-
         for obj in objs:
             bbox = obj.find('bndbox')
-            # Make pixel indexes 0-based
+            # Make pixel indices 0-based
             x1 = max(min((float(bbox.find('xmin').text) - 1) * w_ratio, self.image_size - 1), 0)
             y1 = max(min((float(bbox.find('ymin').text) - 1) * h_ratio, self.image_size - 1), 0)
             x2 = max(min((float(bbox.find('xmax').text) - 1) * w_ratio, self.image_size - 1), 0)
@@ -139,6 +111,8 @@ class pascal_voc(object):
             boxes = [(x2 + x1) / 2.0, (y2 + y1) / 2.0, x2 - x1, y2 - y1]
             x_ind = int(boxes[0] * self.cell_size / self.image_size)
             y_ind = int(boxes[1] * self.cell_size / self.image_size)
+
+            # one cell must have only one ground truth
             if label[y_ind, x_ind, 0] == 1:
                 continue
             label[y_ind, x_ind, 0] = 1
@@ -146,3 +120,31 @@ class pascal_voc(object):
             label[y_ind, x_ind, 5 + cls_ind] = 1
 
         return label, len(objs)
+        
+    def get(self):
+        images = np.zeros(
+            (self.batch_size, self.image_size, self.image_size, 3))
+        labels = np.zeros(
+            (self.batch_size, self.cell_size, self.cell_size, 25))
+        count = 0
+        while count < self.batch_size:
+            imname = self.gt_labels[self.cursor]['imname']
+            flipped = self.gt_labels[self.cursor]['flipped']
+            images[count, :, :, :] = self.image_read(imname, flipped)
+            labels[count, :, :, :] = self.gt_labels[self.cursor]['label']
+            count += 1
+            self.cursor += 1
+            if self.cursor >= len(self.gt_labels):
+                np.random.shuffle(self.gt_labels)
+                self.cursor = 0
+                self.epoch += 1
+        return images, labels
+        
+    def image_read(self, imname, flipped=False):
+        image = cv2.imread(imname)
+        image = cv2.resize(image, (self.image_size, self.image_size))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image = (image / 255.0) * 2.0 - 1.0
+        if flipped:
+            image = image[:, ::-1, :]
+        return image
